5690e173555b94e8765657b09b9e3482319d3ba6
Iliyan Germanov <iliyan.germanov971@gmail.com>
2023-05-05 17:45:51 +0300

    Add DSL marker

--- src/commonMain/kotlin/arrow.exact/ExactBuilderImpl.kt
+++ src/commonMain/kotlin/arrow.exact/ExactBuilderImpl.kt
@@ -5,32 +5,36 @@ import arrow.core.raise.Raise
 import arrow.core.raise.recover
 import kotlin.jvm.JvmInline
 
+@DslMarker
+annotation class ExactBuilderDsl
+
+@ExactBuilderDsl
 fun <A, Constraint> exactBuilder(
   block: (ExactBuilder<A, Constraint>) -> Either<ExactError, Constraint>
 ): Exact<A, Constraint> {
   return object : Exact<A, Constraint> {
     override fun from(value: A): Either<ExactError, Constraint> =
-      block(ExactBuilderDsl(value.right()))
+      block(ExactBuilderImpl(value.right()))
   }
 }
 
-class ExactBuilderDsl<A, Constraint>(
+private class ExactBuilderImpl<A, Constraint>(
   private val value: Either<ExactError, A>
 ) : ExactBuilder<A, Constraint> {
 
-  override fun mustBe(predicate: Predicate<A>): ExactBuilder<A, Constraint> = ExactBuilderDsl(
+  override fun mustBe(predicate: Predicate<A>): ExactBuilder<A, Constraint> = ExactBuilderImpl(
     value = value.flatMap {
       if (predicate(it)) it.right() else ExactError("Predicate failed").left()
     }
   )
 
-  override fun <B> transform(transformation: (A) -> B): ExactBuilder<B, Constraint> = ExactBuilderDsl(
+  override fun <B> transform(transformation: (A) -> B): ExactBuilder<B, Constraint> = ExactBuilderImpl(
     value = value.map(transformation)
   )
 
   override fun <B> transformOrRaise(
     transformation: Raise<ExactError>.(A) -> B
-  ): ExactBuilder<B, Constraint> = ExactBuilderDsl(
+  ): ExactBuilder<B, Constraint> = ExactBuilderImpl(
     value = value.flatMap {
       recover({ transformation(it).right() }) { ExactError("Transform or raise failed").left() }
     }
@@ -40,9 +44,16 @@ class ExactBuilderDsl<A, Constraint>(
 }
 
 interface ExactBuilder<A, Constraint> {
+  @ExactBuilderDsl
   fun mustBe(predicate: Predicate<A>): ExactBuilder<A, Constraint>
+
+  @ExactBuilderDsl
   fun <B> transform(transformation: (A) -> B): ExactBuilder<B, Constraint>
+
+  @ExactBuilderDsl
   fun <B> transformOrRaise(transformation: Raise<ExactError>.(A) -> B): ExactBuilder<B, Constraint>
+
+  @ExactBuilderDsl
   fun build(constructor: (A) -> Constraint): Either<ExactError, Constraint>
 }
 
