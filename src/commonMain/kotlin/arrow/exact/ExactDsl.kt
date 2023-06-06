6b9f1e083bd2b2ab29d5e6dc226b7089c6391821
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-06-06 08:48:20 +0200

    Reduce ensure methods

--- src/commonMain/kotlin/arrow/exact/ExactDsl.kt
+++ src/commonMain/kotlin/arrow/exact/ExactDsl.kt
@@ -1,6 +1,7 @@
 package arrow.exact
 
 import arrow.core.Either
+import arrow.core.getOrElse
 import arrow.core.raise.Raise
 import arrow.core.raise.RaiseDSL
 
@@ -10,20 +11,9 @@ public inline fun Raise<ExactError>.ensure(condition: Boolean) {
 }
 
 @RaiseDSL
-public inline fun <A, B> Raise<ExactError>.ensure(raw: A, exact: Exact<A, B>): B = ensure(raw, exact as ExactEither<*, A, B>)
-
-@RaiseDSL
 public inline fun <A, B> Raise<ExactError>.ensure(raw: A, exact: ExactEither<*, A, B>): B =
   ensure(raw, exact) { ExactError("Failed to match Exact.") }
 
 @RaiseDSL
-public inline fun <A, B, Error : Any> Raise<Error>.ensure(raw: A, exact: Exact<A, B>, error: (ExactError) -> Error): B =
-  ensure(raw, exact as ExactEither<ExactError, A, B>, error)
-
-@RaiseDSL
-public inline fun <A, B, Error : Any, E : Any> Raise<E>.ensure(raw: A, exact: ExactEither<Error, A, B>, error: (Error) -> E): B {
-  return when (val result = exact.from(raw)) {
-    is Either.Left -> raise(error(result.value))
-    is Either.Right -> result.value
-  }
-}
+public inline fun <A, B, Error : Any, E : Any> Raise<E>.ensure(raw: A, exact: ExactEither<Error, A, B>, error: (Error) -> E): B =
+  exact.from(raw) getOrElse { raise(error(it)) }
