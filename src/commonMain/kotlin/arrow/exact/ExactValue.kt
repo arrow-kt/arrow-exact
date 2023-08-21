98a0622e5f46a2d69bb77489acd84205dc33beb5
0pg <jahun1105@naver.com>
2023-08-21 14:44:57 +0900

    Change access modifier

--- src/commonMain/kotlin/arrow/exact/ExactValue.kt
+++ src/commonMain/kotlin/arrow/exact/ExactValue.kt
@@ -6,7 +6,7 @@ import kotlin.jvm.JvmInline
 public typealias ExactValueSpec<T> = Exact<T, T>
 
 @JvmInline
-public value class ExactValue<T, Cond> private constructor(public val value: T) {
+public value class ExactValue<T, Cond> private constructor(private val value: T) {
     public companion object {
         public fun <T, Cond : ExactValueSpec<T>> fromSpec(
             value: T,
